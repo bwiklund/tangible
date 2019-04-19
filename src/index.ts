ec18676027c433e9c9fddd4ce10691e93351aef7
Ben Wiklund <ben@daisyowl.com>
2019-04-18 18:41:13 -0700

    render

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ interface Material {
 }
 
 interface MatNode {
-  type: string
+  type: (x: number, y: number) => number
 }
 
 function Noise(x: number, y: number) {
@@ -11,6 +11,8 @@ function Noise(x: number, y: number) {
 }
 
 function renderMaterial(def: Material, w: number, h: number) {
+  var startTime = new Date();
+
   var canvas = document.createElement("canvas");
   document.body.appendChild(canvas);
   canvas.width = w;
@@ -19,21 +21,34 @@ function renderMaterial(def: Material, w: number, h: number) {
   var iData = ctx.getImageData(0, 0, w, h);
   var data = iData.data;
 
+  // TODO: when things get eval'd for the first time, calculate their whole buffer and save it. it's faster.
+  // var nodeCache: { [s: string]: Float32Array } = {};
+
+  var root = def.nodes.root;
+
   for (var y = 0; y < h; y++) {
     for (var x = 0; x < w; x++) {
       var i = x + y * w;
       var I = i * 4;
-      var val = Noise(x, y);
+
+
+      var val = root.type(x, y);
+
       data[I + 0] = val * 255;
       data[I + 1] = val * 255;
       data[I + 2] = val * 255;
       data[I + 3] = 255;
     }
   }
+  
 
   ctx.putImageData(iData, 0, 0);
 
   document.body.appendChild(canvas);
+
+  var endTime = new Date();
+
+  console.log(`Rendered ${w}x${h} material in ${endTime.getTime() - startTime.getTime()}ms`)
 }
 
 
@@ -41,7 +56,7 @@ function renderMaterial(def: Material, w: number, h: number) {
 var testMaterial: Material = {
   nodes: {
     root: {
-      type: "Noise"
+      type: Noise
     }
   }
 }
