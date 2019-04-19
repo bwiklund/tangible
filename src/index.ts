b646e3968d1fe177a73d07ac6de3a623dcd0bb1a
Ben Wiklund <ben@daisyowl.com>
2019-04-18 18:27:18 -0700

    sketchin

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
 console.log("hi");
+
+interface Material {
+  nodes: {[s: string]: MatNode}
+}
+
+interface MatNode {
+  type: string
+}
+
+function Noise(x: number, y: number) {
+  return Math.random();
+}
+
+var testMaterial: Material = {
+  nodes: {
+    root: {
+      type: "Noise"
+    }
+  }
+}
